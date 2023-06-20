2ce3e0e28ab5e59489744d01117af38ab94712a0
9RACHA <66274956+9RACHA@users.noreply.github.com>
2023-06-20 11:28:48 +0200

    Comentarios

--- FruitMonster-20230111/FruitMonster-20230111/Assets/Scripts/GameManager.cs
+++ FruitMonster-20230111/FruitMonster-20230111/Assets/Scripts/GameManager.cs
@@ -3,36 +3,68 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
-    
+
+    // Instancia única del GameManager
     public static GameManager instance;
 
+    // Prefabs de las frutas disponibles en el juego
     public GameObject[] fruitPrefabs;
+
+    // Transform del monstruo en el juego
     public Transform monster;
+
+    // Transform del carrusel de frutas en el juego
     public Transform fruitCarrousel;
+
+    // Punto de generación de las frutas en el juego
     public Transform fruitSpawnPoint;
 
+    // Sonido de fallo al perder una vida
     public AudioClip failAudioClip;
+
+    // Sonido de éxito al capturar una fruta
     public AudioClip winAudioClip;
-    
+
+    // Contador de vidas
     private int lifeCount = 3;
+
+    // Puntuación
     private int score = 0;
+
+    // Bandera de pausa del juego
     private bool paused = false;
+
+    // Bandera de fin de juego
     private bool gameOver = false;
+
+    // Siguiente incremento de velocidad del monstruo
     private int nextSpeedIncrement = 50;
+
+    // Tiempo desde la última captura de fruta
     private float lastCaptureTime = -1;
+
+    // Tiempo en el que se duplica la puntuación
     private float doublePointsTime = 0.8f;
+
+    // Bandera para obtener una vida adicional
     private bool additionalLife = true;
+
+    // Referencia a la fruta actualmente en juego
     private GameObject fruit;
 
+    // Valor temporal para almacenar el tiempo escala anterior
     private float oldTimeScale = 0;
+
+    // Diferencia máxima permitida en ángulo entre el monstruo y la fruta
     private const float MAX_ANGLE_DIFFERENCE = 4f;
 
     void Awake() {
         instance = this;
     }
+
     // Start is called before the first frame update
     void Start() {
-        if(fruitPrefabs ==  null || fruitPrefabs.Length == 0) {
+        if(fruitPrefabs == null || fruitPrefabs.Length == 0) {
             Debug.Log("GameManager: La variable fruitPrefabs no está correctamente inicializada");
         }
         if(monster == null) {
@@ -46,7 +78,6 @@ public class GameManager : MonoBehaviour {
         }
 
         SpawnFruit();
-        
     }
 
     // Update is called once per frame
@@ -69,17 +100,19 @@ public class GameManager : MonoBehaviour {
                 LoseLife();
             }
         }
-
     }
 
+    // Comprueba si el juego está pausado
     public bool IsGamePaused() {
         return paused;
     }
 
+    // Comprueba si el juego ha terminado
     public bool IsGameOver() {
         return gameOver;
     }
 
+    // Captura una fruta con éxito
     void CaptureFruit() {
         GetComponent<AudioSource>().PlayOneShot(winAudioClip);
         score += fruit.GetComponent<Fruit>().points;
@@ -91,15 +124,14 @@ public class GameManager : MonoBehaviour {
         }
 
         lastCaptureTime = Time.time;
-        //Comprobamos si hemos pasado una barrera de puntos múltiplo de 50
+
+        // Comprueba si se ha alcanzado el umbral de incremento de velocidad
         if(score >= nextSpeedIncrement) {
-            //Incrementamos la velocidad del monstruo
+            // Incrementa la velocidad del monstruo
             monster.GetComponent<Monster>().IncrementSpeed();
-            //Fijamos el siguiente umbral de incremento de velocidad
+            // Fija el siguiente umbral de incremento de velocidad
             nextSpeedIncrement += 50;
         }
-
-  
 
         if(additionalLife) {
             if(score >= 100) {
@@ -112,6 +144,7 @@ public class GameManager : MonoBehaviour {
         SpawnFruit();
     }
 
+    // Pierde una vida
     void LoseLife() {
         GetComponent<AudioSource>().PlayOneShot(failAudioClip);
         lifeCount--;
@@ -123,11 +156,10 @@ public class GameManager : MonoBehaviour {
         }
     }
 
-
-
+    // Genera una nueva fruta en el juego
     void SpawnFruit() {
         Vector3 eulerAngles = fruitCarrousel.eulerAngles;
-        eulerAngles.z  = Random.Range(0, 360);
+        eulerAngles.z = Random.Range(0, 360);
         fruitCarrousel.eulerAngles = eulerAngles;
 
         int fruitIndex = Random.Range(0, fruitPrefabs.Length);
@@ -135,8 +167,9 @@ public class GameManager : MonoBehaviour {
         fruit = Instantiate(fruitPrefabs[fruitIndex], fruitSpawnPoint.position, Quaternion.identity);
     }
 
+    // Muestra la puntuación y las vidas en la interfaz gráfica del juego
     void OnGUI() {
-        GUIStyle myStyle = new GUIStyle(); 
+        GUIStyle myStyle = new GUIStyle();
         myStyle.fontSize = 32;
         myStyle.onNormal.textColor = Color.white;
         GUI.Label(new Rect(10, 10, 100, 30), "Vidas: " + lifeCount, myStyle);
--- FruitMonster-20230111/FruitMonster-20230111/Assets/Scripts/Monster.cs
+++ FruitMonster-20230111/FruitMonster-20230111/Assets/Scripts/Monster.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class Monster : MonoBehaviour {
 
+    // Velocidad de rotación del monstruo
     private float rotationSpeed = 100f;
 
     // Update is called once per frame
     void Update() {
+        // Comprobamos si el juego está pausado o si el juego ha terminado
         if(GameManager.instance.IsGamePaused() || GameManager.instance.IsGameOver()) {
             return;
         }
-        transform.Rotate(Vector3.forward, rotationSpeed*Time.deltaTime);
+        // Rotamos el monstruo alrededor de su eje Z utilizando la velocidad de rotación y el tiempo transcurrido desde el último frame
+        transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
     }
 
+    // Método para incrementar la velocidad de rotación del monstruo
     public void IncrementSpeed() {
         rotationSpeed += 20;
     }
